c145c3c71ede976907743fc084946ce18ebf7a86
Arista Soluciones <isc061990@outlook.com>
2019-12-24 00:16:20 -0600

    .

--- sistema/ajax/do-poll.php
+++ sistema/ajax/do-poll.php
@@ -6,7 +6,37 @@ include_once(DOC_ROOT.'/libraries.php');
 session_start();
 switch($_POST["type"]){
     case "saveEncuestaVictima":
+        $question->setEncuestaId($_POST["pollId"]);
+        if($_POST["pollVictimaId"])
+            $question->setPollVictimaId($_POST["pollVictimaId"]);
 
+        if($question->saveCurrentAnswersVictima()){
+            echo "ok[#]";
+            echo $error->ShowErrors();
+            echo "[#]";
+            echo $_POST["pollId"];
+            echo "[#]";
+            echo $question->getPollVictimaId();
+        }else{
+            echo "fail[#]";
+            echo $error->ShowErrors();
+            echo "[#]";
+            echo $_POST["pollId"];
+        }
 
+    break;
+    case "showResultado":
+        $question->setVictimaId($_POST["victimaId"]);
+        $question->setContexto($_POST["tipoContexto"]);
+        if($question->validateFullResolvePoll()){
+            echo "ok[#]";
+            $smarty->assign("complete",1);
+            $smarty->assign("victimaId",$_POST["victimaId"]);
+            $smarty->display(DOC_ROOT."/templates/boxes/show-options-result.tpl");
+        }else{
+            echo "fail[#]";
+            $smarty->assign("complete",0);
+            $smarty->display(DOC_ROOT."/templates/boxes/show-options-result.tpl");
+        }
     break;
 }
